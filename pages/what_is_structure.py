f0895f36ea22f5c49da3eb3e74d7f8f66f45bbc7
furthermares <furthermares@gmail.com>
2023-07-26 17:28:06 +0900

    format extension to lowercase

--- pages/what_is_structure.py
+++ pages/what_is_structure.py
@@ -45,8 +45,9 @@ file_image_dict = {
 }
 
 def get_file_icon_url(file_extension):
+    file_extension = file_extension.lower()
     if file_extension in file_image_dict:
-        return f"https://raw.githubusercontent.com/PKief/vscode-material-icon-theme/main/icons/{file_image_dict[file_extension.lower()]}.svg"
+        return f"https://raw.githubusercontent.com/PKief/vscode-material-icon-theme/main/icons/{file_image_dict[file_extension]}.svg"
     else:
         return ""
 
@@ -103,6 +104,7 @@ file_type_dictionary = {
 
 def get_markdown_language_form(file_name):
     extension_name = file_name.split(".")[-1]
+    extension_name = extension_name.lower()
     if extension_name in file_image_dict:
         return file_image_dict[extension_name]
     else:
